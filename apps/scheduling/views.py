723bfcc9ddbba4d16197eac64de240e103bcbc4c
alireyhan <ali.rehan2842@gmail.com>
2026-02-03 17:03:41 +0500

    Added imports for `ValidationError` and `PermissionDenied` exceptions.

--- apps/scheduling/views.py
+++ apps/scheduling/views.py
@@ -3,6 +3,7 @@ from rest_framework.decorators import action
 from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.exceptions import ValidationError, PermissionDenied
 from django.db.models import Q
 
 from .models import Session, Booking, StaffClientAssignment, PricingOption, ClientPass
