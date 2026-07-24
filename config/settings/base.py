42ddc1eba9ca1345b82556dbab109fa64b1ca48e
RanaAns07 <ranaans.pk@gmail.com>
2026-07-25 01:54:37 +0500

    chore: add media base settings configuration

--- config/settings/base.py
+++ config/settings/base.py
@@ -93,9 +93,11 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+
 # Media files (Uploaded by users)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
+
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
