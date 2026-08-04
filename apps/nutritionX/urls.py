8fdea3dc9bb1b963d1aa7ed0e8712275873e9dde
RanaAns07 <ranaans.pk@gmail.com>
2026-08-04 21:04:35 +0500

    fix(nutritionX): clean up routes by removing double nutrition prefix completely

--- apps/nutritionX/urls.py
+++ apps/nutritionX/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from apps.nutritionX.views import AddFoodToMealView, ClientMacroHistoryListView, CustomBeverageAPIView, DailyProgressAPIView, NutritionGoalBulkCreateAPIView, UserDailyMealsView, CustomFoodEntryAPIView, UserDrinkHistoryAPIView, WaterIntakeAPIView
 
 urlpatterns = [
-    path("nutrition/add-food/", AddFoodToMealView.as_view(), name="add-food"),
-    path("nutrition/daily-meals/", UserDailyMealsView.as_view(), name="daily-meals"),
+    path("add-food/", AddFoodToMealView.as_view(), name="add-food"),
+    path("daily-meals/", UserDailyMealsView.as_view(), name="daily-meals"),
     path("custom-meal/", CustomFoodEntryAPIView.as_view(), name="meal-log-create"),
 
-    path("nutrition/water-intake/", WaterIntakeAPIView.as_view(), name="water-intake"),
+    path("water-intake/", WaterIntakeAPIView.as_view(), name="water-intake"),
     path("goals/", NutritionGoalBulkCreateAPIView.as_view(), name="nutrition-goals"),
     path("add-custom-beverage/", CustomBeverageAPIView.as_view(), name="add-custom-beverage"),
     path('custom-beverages/<int:pk>/', CustomBeverageAPIView.as_view(), name='custom-beverage-detail'),
